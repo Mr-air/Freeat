0f2823d37b626530849e5c12a54594525e3514f0
unknown <you@example.com>
2020-06-21 05:15:33 +0300

    V05

--- src/panels/support.js
+++ src/panels/support.js
@@ -55,15 +55,17 @@ export default class Hello extends Component {
             <progress className="progress-users" max="30" value="12"></progress> <span>12/30</span>
         </div>
         <div className="bottom__block--buttons">
-            <Button> Личный диетолог
-                <a href="https://vk.com/dietalegko.online"></a>
-            </Button>
-            <Button> Помощь психолога
-                <a href="https://neurolex-center.ru"></a>
-            </Button>
-            <Button> Результаты Анализа
-                <a href="https://docviewer.yandex.ru/view/243740326/?*=6SLRDQRKZQQJVzzwiEQeg5LHGUV7InVybCI6Imh0dHA6Ly93d3cucHNldWRvbG9neS5vcmcvaGVhbHRoL0luZ2VybGVpYl9Qb2xueXlfc3ByYXZvY2huaWtfYW5hbGl6b3YyLnBkZiIsInRpdGxlIjoiSW5nZXJsZWliX1BvbG55eV9zcHJhdm9jaG5pa19hbmFsaXpvdjIucGRmIiwibm9pZnJhbWUiOnRydWUsInVpZCI6IjI0Mzc0MDMyNiIsInRzIjoxNTkyNzAzODQzNTYwLCJ5dSI6IjMwNjkyNzY5NjE1NTQ4MDM4NDMiLCJzZXJwUGFyYW1zIjoibGFuZz1ydSZ0bT0xNTkyNzAzODM3JnRsZD1ydSZuYW1lPUluZ2VybGVpYl9Qb2xueXlfc3ByYXZvY2huaWtfYW5hbGl6b3YyLnBkZiZ0ZXh0PSVEMCVCMCVEMCVCRCVEMCVCMCVEMCVCQiVEMCVCOCVEMCVCNyVEMSU4QislRDAlQkYlRDAlQjQlRDElODQmdXJsPWh0dHAlM0EvL3d3dy5wc2V1ZG9sb2d5Lm9yZy9oZWFsdGgvSW5nZXJsZWliX1BvbG55eV9zcHJhdm9jaG5pa19hbmFsaXpvdjIucGRmJmxyPTExMDA4Jm1pbWU9cGRmJmwxMG49cnUmc2lnbj01MTQyNWI3M2E3M2ZmMTM0ZmFkY2I4MTZiMzZlNTgxMSZrZXlubz0wIn0%3D&lang=ru"></a>
-            </Button>
+            
+                <a href="https://vk.com/dietalegko.online"><Button>Помощь диетолога</Button></a>
+            
+            
+                <a href="https://neurolex-center.ru"><Button> Помощь психолога</Button></a>
+           
+            
+                <a href="https://docviewer.yandex.ru/view/243740326/?*=6SLRDQRKZQQJVzzwiEQeg5LHGUV7InVybCI6Imh0dHA6Ly93d3cucHNldWRvbG9neS5vcmcvaGVhbHRoL0luZ2VybGVpYl9Qb2xueXlfc3ByYXZvY2huaWtfYW5hbGl6b3YyLnBkZiIsInRpdGxlIjoiSW5nZXJsZWliX1BvbG55eV9zcHJhdm9jaG5pa19hbmFsaXpvdjIucGRmIiwibm9pZnJhbWUiOnRydWUsInVpZCI6IjI0Mzc0MDMyNiIsInRzIjoxNTkyNzAzODQzNTYwLCJ5dSI6IjMwNjkyNzY5NjE1NTQ4MDM4NDMiLCJzZXJwUGFyYW1zIjoibGFuZz1ydSZ0bT0xNTkyNzAzODM3JnRsZD1ydSZuYW1lPUluZ2VybGVpYl9Qb2xueXlfc3ByYXZvY2huaWtfYW5hbGl6b3YyLnBkZiZ0ZXh0PSVEMCVCMCVEMCVCRCVEMCVCMCVEMCVCQiVEMCVCOCVEMCVCNyVEMSU4QislRDAlQkYlRDAlQjQlRDElODQmdXJsPWh0dHAlM0EvL3d3dy5wc2V1ZG9sb2d5Lm9yZy9oZWFsdGgvSW5nZXJsZWliX1BvbG55eV9zcHJhdm9jaG5pa19hbmFsaXpvdjIucGRmJmxyPTExMDA4Jm1pbWU9cGRmJmwxMG49cnUmc2lnbj01MTQyNWI3M2E3M2ZmMTM0ZmFkY2I4MTZiMzZlNTgxMSZrZXlubz0wIn0%3D&lang=ru">
+                <Button> Результаты Анализа </Button> 
+                </a>
+            
         </div>
         </>
     );
